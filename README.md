67f642c89cc89383523c6e18ed06675262b64c48
Robert Vunabandi <rvunabandi@gmail.com>
2018-08-23 23:35:42 -0700

    added files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # website
 Official TDC rush website. 
+
+## Things in This Repo
+
+### NPM
+
+`package.json`
+`package-lock.json`
+
+### Bower
+
+`.bowerrc`
+`bower.json`
+
+### ESLint
+
+`.eslintrc.json`
+
+### GitIgnore
+
+`.gitignore`
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// see https://realpython.com/the-ultimate-flask-front-end/#react-round-one
+/* global require */
+"use strict";
+const gulp = require("gulp");
+const del = require("del");
+const size = require("gulp-size");
+const babel = require("gulp-babel");
+
+gulp.task("del", function () {
+  return del(["./app/static/js"]);
+});
+
+gulp.task("transform", function () {
+  return gulp.src("./static/jsx/*.jsx")
+    .pipe(size())
+    .pipe(babel({ plugins: ["transform-react-jsx"] }))
+    .pipe(gulp.dest("./static/js/"));
+});
+
+gulp.task(
+  "default",
+  ["del"],
+  function () {
+    gulp.start("transform");
+    gulp.watch("./static/jsx/*.jsx", ["transform"]);
+  }
+);
